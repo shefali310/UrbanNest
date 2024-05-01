96769eca17b41d918d337b09ae03dd5b0754eb0c
Shefali Panchal <61265165+shefali310@users.noreply.github.com>
2024-05-01 14:20:37 -0400

    Back End Final Commit

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
+import myHotelRoutes from "./routes/my-hotels";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import hotelRoutes from "./routes/hotels";
+import bookingRoutes from "./routes/my-bookings";
+import usersWithBookingsRoutes from "./routes/my-users";
+import path from "path";
+
+// Cloudinary setup
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Mongodb connection
+const connectDB = async () => {
+  return await mongoose.connect(`${process.env.MONGODB_CONNECTION_STRING}`);
+};
+
+connectDB()
+  .then(() => {
+    console.log("*****Database Connected*****");
+  })
+  .then(() => {
+    app.listen(3002, () => {
+      console.log("Server running on localhost:3002");
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB Error : ", error);
+    process.exit(1);
+  });
+
+//  starting express server
+const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+// app.use(express.static(path.join(__dirname, "../../front-end/dist")))
+
+// Api handling
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
+app.use("/api/hotels", hotelRoutes);
+app.use("/api/my-bookings", bookingRoutes);
+app.use("/api/users-with-bookings", usersWithBookingsRoutes);
