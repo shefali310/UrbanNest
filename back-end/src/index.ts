aca4414219fbc5f222f04d5cf7a1cdc241f74df6
Shefali Panchal <61265165+shefali310@users.noreply.github.com>
2024-05-30 22:49:00 -0400

    cors changed

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -46,7 +46,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
   })
 );
